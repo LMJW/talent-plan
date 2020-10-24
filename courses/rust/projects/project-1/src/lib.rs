cb55dfca20a2cbcf749717a3da70ccc7285f5973
LMJW <heysuperming@gmail.com>
2020-10-25 10:58:15 +1100

    Setup Project 1

---
+++ courses/rust/projects/project-1/src/lib.rs
@@ -0,0 +1,21 @@
+pub struct KvStore{
+    // todo
+}
+
+impl KvStore{
+    pub fn new() ->Self{
+        todo!()
+    }
+
+    pub fn get(&self, key: String) ->Option<String> {
+        todo!()
+    }
+
+    pub fn set(&mut self, key: String, val:String)  {
+        todo!()
+    }
+
+    pub fn remove(&mut self, key:String) {
+        todo!()
+    }
+}
